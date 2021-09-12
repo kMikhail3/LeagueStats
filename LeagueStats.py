8fabc5cbf498089d3597056ea8307bb16840c3cf
kMikhail3 <86896683+kMikhail3@users.noreply.github.com>
2021-09-12 01:48:39 -0400

    Add files via upload

--- LeagueStats.py
+++ LeagueStats.py
@@ -44,6 +44,11 @@ def sendStats():
 def matchHist():
     page = requests.get(my_url)
     page_soup = soup(page.content, 'html.parser')
+    summoner = page_soup.find('span', class_='Name').text
+
+    pfpUrl = page_soup.find('div',class_="ProfileIcon")
+    pfp = 'https:'+pfpUrl.img.get('src').split("?")[0]
+
     matchContainer = page_soup.find_all('div', class_='GameItemWrap')
     historyArray = []
     for match in matchContainer:    
@@ -57,9 +62,14 @@ def matchHist():
         assists = match.find('span', class_='Assist').text.replace('\t', '')
         KDA_ratio = match.find('span', class_='KDARatio').text.replace('\t', '')
         date = match.find('div',class_='TimeStamp').text
-        champImage = match.find('img',class_="Image")
-        matchHistory = [gameType,gameRes,gameLen,kills,deaths,assists,KDA_ratio]
+        champUrl = match.find('img',class_="Image")
+        champImg = "https:"+champUrl.get('src').split("?")[0]
+    
+        
+
+        matchHistory = [summoner,gameType,gameRes,gameLen,kills,deaths,assists,KDA_ratio,date,champImg,pfp]
         historyArray.append(matchHistory)
+    
     return(historyArray)
 
 ####         ####
@@ -71,7 +81,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!")
 
 prefix = '!'
 
@@ -82,53 +92,89 @@ def changePrefix (newPrefix):
 
 def setUser (leagueName):
     global my_url 
-    my_url = my_url + leagueName
+    new_url = 'https://na.op.gg/summoner/userName=' + leagueName
+    my_url = new_url
 
-bot = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
     print("I'm ready {0.user}.".format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-         return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#          return
     
-    if message.content.startswith(prefix+'hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith(prefix+'hello'):
+#         await message.channel.send('Hello!')
 
-    if message.content.startswith(prefix+'prefix'):
-        if message.content[8].isalnum(): 
-            await message.channel.send("Prefix must be one symbol e.g. !, $, <, >, %...")
-            pass
-        else:
-            changePrefix(message.content[8])
-            await message.channel.send('The prefix has been changed to ' + message.content[8])
-            print(prefix)
+#     if message.content.startswith(prefix+'prefix'):
+#         if message.content[8].isalnum(): 
+#             await message.channel.send("Prefix must be one symbol e.g. !, $, <, >, %...")
+#             pass
+#         else:
+#             changePrefix(message.content[8])
+#             await message.channel.send('The prefix has been changed to ' + message.content[8])
+#             print(prefix)
 
-    if message.content.startswith(prefix+'stats'):
-        lolName = message.content.split(" ")
-        await message.channel.send(lolName[1])
-        setUser(lolName[1])
-        playerStats = sendStats()
-        await message.channel.send(playerStats)
+#     if message.content.startswith(prefix+'stats'):
+#         lolName = message.content.split(" ")
+#         await message.channel.send(lolName[1])
+#         setUser(lolName[1])
+#         playerStats = sendStats()
+#         await message.channel.send(playerStats)
 
-    if message.content.startswith(prefix+'test'):
-        await message.channel.send(my_url)
+#     if message.content.startswith(prefix+'test'):
+#         await message.channel.send(my_url)
      
-    if message.content.startswith(prefix+'send'):
-        playerStats2 = sendStats()
-        await message.channel.send(playerStats2)
-
-    if message.content.startswith(prefix+'history'):
-        lolName = message.content.split(" ")
-        await message.channel.send(lolName[1])
-        setUser(lolName[1])
-        matchHistory = matchHist()
-        await message.channel.send(matchHistory)
-        
+#     if message.content.startswith(prefix+'send'):
+#         playerStats2 = sendStats()
+#         await message.channel.send(playerStats2)
 
 
+def colourCheck(matchHistory, game):
+    if matchHistory[game][2] == "Victory":
+        return discord.Colour.blue()
+    else: 
+        return discord.Colour.red()
 
+@client.command()
+async def history(ctx, lolName):
+    await ctx.send(lolName)
+    setUser(lolName)
+    matchHistory = matchHist()
+    #await ctx.send(matchHistory)
+
+    # await ctx.send(matchAmount)
+    # if matchAmount == "()":
+    #     matchAmount = len(matchHistory)
+    # if not str(matchAmount).isnumeric():
+    #     ctx.send('Please specify a valid number of matches')
+    #     return 0
+    # if matchAmount > len(matchHistory):
+    #     matchAmount = len(matchHistory)
+
+   # matchHistory = [summoner,gameType,gameRes,gameLen,kills,deaths,assists,KDA_ratio,date,champImg]
+
+
+    for game in range (len(matchHistory)):
+        embed = discord.Embed(
+        title = (matchHistory[game][1]+" "+matchHistory[game][2]),
+        description= ("**Game Duration**"+"```"+matchHistory[game][3])+"```",
+        colour = colourCheck(matchHistory, game)
+        )
+
+
+        embed.set_thumbnail(url=matchHistory[game][9])
+        embed.set_author(name=matchHistory[game][0], icon_url=matchHistory[game][10], url=my_url)
+        embed.add_field(name='Kills', value="```"+matchHistory[game][4]+"```", inline=True)
+        embed.add_field(name='Deaths', value="```"+matchHistory[game][5]+"```", inline=True)
+        embed.add_field(name='Assists', value="```"+matchHistory[game][6]+"```", inline=True)
+        embed.add_field(name='KDA', value="```"+matchHistory[game][7]+"```", inline=True)
+
+        embed.set_footer(text=matchHistory[game][8])
+        await ctx.send(embed=embed)
+    
+       
 client.run(TOKEN)
